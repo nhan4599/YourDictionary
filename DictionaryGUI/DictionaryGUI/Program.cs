50a40f3b36327b8773d956124a243d350c869caa
minhtamtkst21 <minhtamtruongkhanh.st@gmail.com>
2019-10-17 15:05:04 +0700

    Designing GUI

--- DictionaryGUI/DictionaryGUI/Program.cs
+++ DictionaryGUI/DictionaryGUI/Program.cs
@@ -16,7 +16,7 @@ namespace DictionaryGUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new GUI());
         }
     }
 }
