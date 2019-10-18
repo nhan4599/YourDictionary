181c88f0e3ae9d14201f1db13e1a7f5dde15b7b4
nguyen tri nhan ho <nhan01628483955@gmail.com>
2019-10-18 21:54:14 +0700

    Designing GUI

--- DictionaryGUI/DictionaryGUI/GUI.Designer.cs
+++ DictionaryGUI/DictionaryGUI/GUI.Designer.cs
@@ -1,4 +1,7 @@
-﻿namespace DictionaryGUI
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace DictionaryGUI
 {
     partial class GUI
     {
@@ -28,41 +31,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tabs = new System.Windows.Forms.TabControl();
-            this.tabSearch = new System.Windows.Forms.TabPage();
-            this.tabManager = new System.Windows.Forms.TabPage();
-            this.split = new System.Windows.Forms.SplitContainer();
+            this.tabs = new TabControl();
+            this.tabSearch = new TabPage();
+            this.tabManager = new TabPage();
+            this.pnlTop = new Panel();
+            this.split = new SplitContainer();
             this.tabs.SuspendLayout();
             this.tabSearch.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.split)).BeginInit();
             this.split.SuspendLayout();
             this.SuspendLayout();
 
-            //
-            // Split
-            //
-            this.split.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.split.Orientation = System.Windows.Forms.Orientation.Horizontal;
-
             // 
             // tabs
             // 
             this.tabs.Controls.Add(this.tabSearch);
             this.tabs.Controls.Add(this.tabManager);
-            this.tabs.Location = new System.Drawing.Point(0, 0);
+            this.tabs.Location = new Point(0, 0);
             this.tabs.Name = "tabs";
             this.tabs.SelectedIndex = 0;
-            this.tabs.Size = new System.Drawing.Size(600, 600);
+            this.tabs.Size = new Size(600, 600);
             this.tabs.TabIndex = 0;
 
             // 
             // tabSearch
             // 
             this.tabSearch.Controls.Add(this.split);
-            this.tabSearch.Location = new System.Drawing.Point(4, 25);
+            this.tabSearch.Controls.Add(this.pnlTop);
+            this.tabSearch.Location = new Point(4, 25);
             this.tabSearch.Name = "tabSearch";
-            this.tabSearch.Padding = new System.Windows.Forms.Padding(3);
-            this.tabSearch.Size = new System.Drawing.Size(592, 571);
+            this.tabSearch.Padding = new Padding(3);
+            this.tabSearch.Size = new Size(592, 571);
             this.tabSearch.TabIndex = 0;
             this.tabSearch.Text = "Search";
             this.tabSearch.UseVisualStyleBackColor = true;
@@ -70,37 +69,45 @@
             // 
             // tabManager
             // 
-            this.tabManager.Location = new System.Drawing.Point(4, 25);
+            this.tabManager.Location = new Point(4, 25);
             this.tabManager.Name = "tabManager";
-            this.tabManager.Padding = new System.Windows.Forms.Padding(3);
-            this.tabManager.Size = new System.Drawing.Size(592, 571);
+            this.tabManager.Padding = new Padding(3);
+            this.tabManager.Size = new Size(592, 571);
             this.tabManager.TabIndex = 1;
             this.tabManager.Text = "Manager Word";
             this.tabManager.UseVisualStyleBackColor = true;
 
             // 
+            // panel1
+            // 
+            this.pnlTop.BorderStyle = BorderStyle.FixedSingle;
+            this.pnlTop.Location = new Point(0, 0);
+            this.pnlTop.Name = "panel1";
+            this.pnlTop.Size = new Size(596, 74);
+            this.pnlTop.TabIndex = 0;
+
+            // 
             // splitContainer1
             // 
-            this.split.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.split.Location = new System.Drawing.Point(3, 3);
+            this.split.Location = new Point(0, 74);
             this.split.Name = "splitContainer1";
-            this.split.Size = new System.Drawing.Size(586, 565);
-            this.split.SplitterDistance = 195;
-            this.split.TabIndex = 0;
+            this.split.Size = new Size(592, 497);
+            this.split.SplitterDistance = 197;
+            this.split.TabIndex = 1;
 
             // 
             // GUI
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 600);
+            this.AutoScaleDimensions = new SizeF(8F, 16F);
+            this.AutoScaleMode = AutoScaleMode.Font;
+            this.ClientSize = new Size(600, 600);
             this.Controls.Add(this.tabs);
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Margin = new Padding(4);
             this.MaximizeBox = false;
             this.MaximumSize = this.Size;
             this.MinimumSize = this.Size;
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Name = "GUI";
+            this.StartPosition = FormStartPosition.CenterScreen;
             this.Text = "My Dictionary";
             this.tabs.ResumeLayout(false);
             this.tabSearch.ResumeLayout(false);
@@ -111,10 +118,11 @@
 
         #endregion
 
-        private System.Windows.Forms.TabControl tabs;
-        private System.Windows.Forms.TabPage tabSearch;
-        private System.Windows.Forms.TabPage tabManager;
-        private System.Windows.Forms.SplitContainer split;
+        private TabControl tabs;
+        private TabPage tabSearch;
+        private TabPage tabManager;
+        private SplitContainer split;
+        private Panel pnlTop;
     }
 }
 
